6e90264393d0bbbc7ddcda11ed5035c0ff4de3cc
Tony Berger <90921536+User234586@users.noreply.github.com>
2025-02-12 21:18:18 +0100

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -7,7 +7,7 @@ variable "CIDR_BLOCK" {
 }
 
 variable "AWS_REGION" { 
-  default = "us-east-1"
+  default = "us-west-2"
   description = "AWS Region"  
 }
 
@@ -15,7 +15,7 @@ variable "AMIs" {
   type = map(string)
   description = "Region specific AMI"
   default = {
-    us-east-1 = "ami-0230bd60aa48260c6"
+    us-west-2 = "ami-0230bd60aa48260c6"
     eu-central-1 = "ami-0ec8c354f85e48227"
   }
 }
