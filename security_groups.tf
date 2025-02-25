e2eb5874a03f6995cbe190f171f72e288ea45114
Tony Berger <90921536+User234586@users.noreply.github.com>
2025-02-25 23:13:54 +0100

    Update security_groups.tf

--- security_groups.tf
+++ security_groups.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["<YOUR_IP_ADDRESS>/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
